1175e4d67f90a2fc2253428acce223234564c2ec
hconn7 <henryconner10@gmail.com>
2025-04-24 15:40:43 -0700

    Proxy

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,5 +3,9 @@ package main
 import "github.com/hconn7/vxp/internal/server"
 
 func main() {
-	server.Start()
+	go server.StartServer("8000")
+	go server.StartServer("8002")
+	go server.StartServer("8001")
+
+	select {}
 }
